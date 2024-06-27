6860cec741c9c9646f58e8dc1ef249523b2e8e81
Guilherme <guiglreis@gmail.com>
2024-06-27 00:55:03 -0300

    .

--- PEDIDO/Pedido.js
+++ PEDIDO/Pedido.js
@@ -1,19 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const debitForm = document.getElementById('debitForm');
-    const creditForm = document.getElementById('creditForm');
-    const debitBtn = document.getElementById('debitBtn');
-    const creditBtn = document.getElementById('creditBtn');
+const debitForm = document.getElementById('debitForm');
+const creditForm = document.getElementById('creditForm');
+const debitBtn = document.getElementById('debitBtn');
+const creditBtn = document.getElementById('creditBtn');
 
-    debitBtn.addEventListener('click', () => {
-        debitForm.classList.remove('hiddenForm');
-        creditForm.classList.add('hiddenForm');
-    });
+debitBtn.addEventListener('click', function() {
+    debitForm.style.display = 'block';
+    creditForm.style.display = 'none';
+});
 
-    creditBtn.addEventListener('click', () => {
-        creditForm.classList.remove('hiddenForm');
-        debitForm.classList.add('hiddenForm');
-    });
-
-    debitForm.classList.add('hiddenForm');
-    creditForm.classList.add('hiddenForm');
+creditBtn.addEventListener('click', function() {
+    creditForm.style.display = 'block';
+    debitForm.style.display = 'none';
 });
