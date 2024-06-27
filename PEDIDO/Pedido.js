27cb39c33ae1d97ccd8e180fbc37357008f00084
Guilherme <guiglreis@gmail.com>
2024-06-27 00:42:08 -0300

    .

--- PEDIDO/Pedido.js
+++ PEDIDO/Pedido.js
@@ -4,19 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const debitBtn = document.getElementById('debitBtn');
     const creditBtn = document.getElementById('creditBtn');
 
-    // Função para mostrar o formulário de débito ao clicar no botão Débito
     debitBtn.addEventListener('click', () => {
         debitForm.classList.remove('hiddenForm');
         creditForm.classList.add('hiddenForm');
     });
 
-    // Função para mostrar o formulário de crédito ao clicar no botão Crédito
     creditBtn.addEventListener('click', () => {
         creditForm.classList.remove('hiddenForm');
         debitForm.classList.add('hiddenForm');
     });
 
-    // Adicione a classe hiddenForm aos formulários de débito e crédito para ocultá-los inicialmente
     debitForm.classList.add('hiddenForm');
     creditForm.classList.add('hiddenForm');
 });
